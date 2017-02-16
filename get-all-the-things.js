c72eb80fbf7f4d3db8930bc0c9c4679ae9ee1f42
MichaelCook <michael@evoluted.net>
2017-02-16 14:27:44 +0000

    Boom, production, let's do this

--- get-all-the-things.js
+++ get-all-the-things.js
@@ -1,10 +1,14 @@
 let fs = require('fs'),
-	fileList ='../../get-all-these-things.json';
+	wget = require('wget-improved'),
+	mkdirp = require('mkdirp'),
+	downloaders = [],
+	//root = '../../', // no need - running `node node_modules/..` is relative
+	fileList = 'get-all-the-things.json';
 
 fs.stat(fileList, function(err, stat) {
 	if (err != null) {
 		console.log('You must have the file: ' + fileList);
-		console.log('Relative to the directory node_modules/get-all-the-things)');
+		console.log('(relative to this directory)');
 		return;
 	}
 
@@ -12,10 +16,89 @@ fs.stat(fileList, function(err, stat) {
 
 	things.forEach(item => {
 		// If desired, append the source filetype to the target filename
-		if (item.retainFiletype == true) {
+		if (item.retainFiletype === true) {
 			let fileSplit = item.source.split('.');
 			item.target += '.' + fileSplit[fileSplit.length - 1];
 		}
-		console.log(item);
+
+		// Remove any invalid characters, such as > which really fewks things up
+		// < > : " ' | ? *
+		item.target = item.target.replace(/<|>|:|"|'|\||\?|\*/g,'');
+
+		console.log('\n' + item.source);
+		console.log('-->\n' + item.target + '\n');
+
+		item.targetDir = require('path').dirname(item.target);
+		if (fs.existsSync(item.targetDir)) {
+			downloaders.push(Downloader(item.source, item.target));
+		} else {
+			console.log("Target folder doesn't exist (creating it)");
+			mkdirp(item.targetDir, function (err) {
+				if (err) {
+					console.log("Couldn't create target path:");
+					console.error(err);
+				}
+				else {
+					downloaders.push(Downloader(item.source, item.target));
+				}
+			});
+		}
+
+		return;
 	});
+
+	// Start all downloads
+	downloaders.map(downloader => {
+		downloader.downloadFile();
+	});
+
+	// Check our progress while any downloads are still going
+	let progressCheck = setInterval(function() {
+		var allDone = true;
+		downloaders.map(downloader => {
+			if (downloader.getProgress() != 100) {
+				allDone = false;
+				console.log(downloader.getProgress() + '% of ' + downloader.getFileSize() + ' for ' + downloader.getTarget());
+			}
+		});
+		if (allDone) {
+			clearInterval(progressCheck);
+			console.log('Done!\n');
+		} else {
+			console.log('');
+		}
+	}, 100);
 });
+
+Downloader = (source, target) => {
+	let download,
+		fileSize = 0,
+		progress = 0;
+	return {
+		downloadFile: () => {
+			download = wget.download(source, target);
+			download.on('error', function(err) {
+				console.log(err);
+			});
+			download.on('start', function(_fileSize) {
+				fileSize = _fileSize;
+				//console.log('Starting download of: ' + Math.round(fileSize / 2014 * 10) / 10 + 'kb');
+			});
+			download.on('end', function(output) {
+				//console.log(output);
+			});
+			download.on('progress', function(_progress) {
+				progress = Math.round(_progress * 100);
+			});
+		},
+		getProgress: () => {
+			return progress;
+		},
+		getTarget: () => {
+			return target;
+		},
+		getFileSize: () => {
+			return Math.round(fileSize / 2014 * 10) / 10 + 'kb';
+		}
+	};
+};
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Get All The Things!
+
+Download listed files from a json array to given targets.
+
+## Why?
+
+Well you might just want to download a big ol' list of files, or possibly something more adventurous?
+
+For example, my site [MichaelCook.tech](http://michaelcook.tech) parses markdown files to create its articles and some of those articles incorporate github project readmes. So, in order to keep those localised readmes nice and fresh, I can run this script to bring down the up-to-date files and slot them into the necessary directories.
+
+## Install
+
+	yarn add get-all-the-things
+
+## Run
+
+	node node_modules/get-all-the-things/get-all-the-things.js
+
+## Configuration
+
+You'll need a `get-all-these-things.json` file in your project's root directory with structure similar to:
+
+	[
+		{
+			"source": "http://example.com/image-1.png",
+			"target": "downloads/image-name",
+			"retainFiletype": true
+		},
+		{
+			"source": "http://example.com/file-1.txt",
+			"target": "downloads/file-name.doc"
+		}
+	]
+
+### These are the available parameters for each item
+
+Option | Type | Default | Description
+------ | ---- | ------- | -----------
+source | string | null | Filepath from which to download
+target | string | null | Local destination filepath
+retainFiletype | bool | false | If the target filepath should be appended with the original file type
